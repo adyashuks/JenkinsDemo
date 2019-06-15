d4b7c70fdf1d62f7970653070de2fbb6c6744ca7
adyashuks <adyashukla123.as@gmail.com>
2019-06-15 17:37:40 +0530

    HelloGirl should be output

---
+++ src/HelloGirl.java
@@ -0,0 +1,9 @@
+
+public class HelloGirl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Girl");
+	}
+
+}
---
+++ src/com/demo/helloworld/PrintAdya.java
@@ -0,0 +1,10 @@
+package com.demo.helloworld;
+
+public class PrintAdya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Adya");
+	}
+
+}
